593055a680194cc9beed2f429c96cc34dbb6f817
Dmitry E. Kislov <kislov@easydan.com>
2021-04-29 07:23:20 +0300

    Add logging messages; tiny refactorings

--- src/cgp/utils.py
+++ src/cgp/utils.py
@@ -5,14 +5,13 @@ Created Date: Sunday March 28th 2021
 Author: Dmitry Kislov
 E-mail: kislov@easydan.com
 -----
-Last Modified: Tuesday, April 6th 2021, 12:24:31 pm
+Last Modified: Thursday, April 29th 2021, 7:13:18 am
 Modified By: Dmitry Kislov
 -----
 Copyright (c) 2021
 """
 
 import logging
-import sys
 from dataclasses import dataclass
 from typing import Any, Optional
 
@@ -29,9 +28,9 @@ _logger = logging.getLogger(__name__)
 @dataclass
 class CGPPlotter:
     filename: str = 'output.pdf'
-    scale: float = 1.0
     active_color: str = '#0000ff'
     passive_color: str = "#ff0000"
+    prog: str = 'dot'
 
     def grid_plot(
         self,
@@ -43,10 +42,12 @@ class CGPPlotter:
             _logger.error("Networkx is not installed.")
             return
         if filename:
+            _logger.info(f"Setting filename: {filename}.")
             self.filename = filename
         phenotype.refresh_node_states()
         graph = nx.DiGraph()
         for ind, n in enumerate(phenotype.inputs):
+            _logger.info(f"Including input node, id: {id(n)}.")
             graph.add_node(n,
                            pos=f"{0},{ind}!",
                            shape="square",
@@ -54,20 +55,26 @@ class CGPPlotter:
 
         for ind, n in enumerate(phenotype.pool):
             if active_only and not n.active:
+                _logger.info(
+                    f"Excluding non-active functional node, id: {id(n)}."
+                )
                 continue
             i, j = phenotype.get_node_row(ind), phenotype.get_node_column(ind)
             graph.add_node(
                 n,
                 stype="filled",
                 shape="circle",
-                label=f"[{i+1},{j+1}]\n{n.short_repr}",
-                fillcolor=self.active_color if n.active else self.passive_color,
+                label=f"[{i + 1}, {j + 1}]\n{n.short_repr}",
+                fillcolor=self.active_color
+                if n.active else self.passive_color,
                 color=self.active_color if n.active else self.passive_color
             )
+            _logger.info(f"Including functional node, id: {id(n)}.")
 
         for ind, n in enumerate(phenotype.outputs):
             graph.add_node(n, shape="square",
                            label=f"out[{ind}]")
+            _logger.info(f"Including output node, id: {id(n)}.")
 
         # add edges
         for n2 in phenotype.pool + phenotype.outputs:
@@ -78,25 +85,14 @@ class CGPPlotter:
                     graph.add_edge(n1, n2, label=f"x{ind+1}")
                 else:
                     graph.add_edge(n1, n2)
+                _logger.info(
+                    "Inserting edge between"
+                    f" node1({id(n1)}) and node2({id(n2)})."
+                )
 
         out = to_agraph(graph)
         out.graph_attr.update(splines=True, overlap=False)
-        out.layout(prog="dot")
-        out.draw(self.filename, prog="dot", args="-Knop -Tpdf")
-
+        out.layout(prog=self.prog)
+        out.draw(self.filename, prog=self.prog, args="-Knop -Tpdf")
+        _logger.info(f"Graph created: {self.filename}.")
         return graph
-
-
-def setup_logging(loglevel, stream=sys.stdout):
-    """Setup basic logging
-
-    Args:
-      loglevel (int): minimum loglevel for emitting messages
-    """
-    logformat = "[%(asctime)s] %(levelname)s:%(name)s:%(message)s"
-    logging.basicConfig(
-        level=loglevel,
-        stream=stream,
-        format=logformat,
-        datefmt="%Y-%m-%d %H:%M:%S"
-    )
